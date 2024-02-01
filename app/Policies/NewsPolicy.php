23524d8e82755689931ef90573cb5cbd855d3e3d
YaroslavOstamatii <yaroslav.ostamatii@gmail.com>
2024-02-01 17:50:03 +0200

    edited admin,user,Policy

--- app/Policies/NewsPolicy.php
+++ app/Policies/NewsPolicy.php
@@ -3,34 +3,9 @@
 namespace App\Policies;
 
 use App\Models\News;
-use App\Models\User;
 
 class NewsPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, News $news): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
     /**
      * Determine whether the user can update the model.
      */
